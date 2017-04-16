94ef09015eeadd53c68780d9e20a062eed123e0b
Iñigo Aguas Ardaiz <electroaguas@hotmail.com>
2017-04-17 01:19:28 +0200

    Implementada la búsqueda de profesores por nombre. Es no es sensible a minúsculas y mayúsculas y busca sobre substrings. Soporta las ñ pero no soporta tildes, habrá que ver en el futuro si esto es necesario. Close #9

--- api/mobile_app/views.py
+++ api/mobile_app/views.py
@@ -12,6 +12,19 @@ class TeacherViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
+    def get_queryset(self):
+        """
+        Método para gestionar la búsqueda de profesores por nombre.
+        Es case insensitive y se encarga de buscar la selección como substrings.
+        :return: Una selección de todos los profesores que cumplen el criterio del nombre.
+        """
+        queryset = Teacher.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+
 class TICViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `detail` actions.
